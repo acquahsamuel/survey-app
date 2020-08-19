d14d460bb3d451261415fe60089022aa744bcab5
acquahsamuel <acquahsamuel1996@gmail.com>
2020-08-19 21:43:58 +0000

    google authentication

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+const {
+    googleClientID,
+    googleClientSecret
+} = require('./config/keys');
+
 const app = express();
 
 
 
-app.get('/', (req, res) => {
-    res.send({
-        Developr : ' Acquah Samuel ',
-        Project : " Startup Survey for Companies "
-    })
-});
+
+passport.use(
+    new GoogleStrategy({
+            clientID: googleClientID,
+            clientSecret: googleClientSecret,
+            callbackURL: '/auth/google/callback',
+        },
+        (accessToken) => {
+            console.log(accessToken);
+        }
+    )
+);
+
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email'],
+    
+}));
+
+app.get('/auth/google/callback' ,passport.authenticate('google'));
 
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT ,()=>{
-   `App is runing on ${PORT} hosted on Heroku` 
+app.listen(PORT, () => {
+    `App is runing on ${PORT} hosted on Heroku`;
 });
-
