ee0bbc77652c16567c7de333b32dce646ce22e06
acquahsamuel <acquahsamuel1996@gmail.com>
2020-08-20 19:47:01 +0000

    google oauth debugging

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+require('./models/User');
 require('./services/passport');
+
+mongoose.connect(keys.mongodbURI || 'mongodb://localhost:27017/emaily-dev', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology : true
+}).then(() => {
+    console.log(`Connection to database successful ${keys.mongodbURI}`)
+}).catch((err) => {
+    console.log(`Sorry issue would be resolved immediately`)
+});
 
 const app = express();
 
 require('./routes/authRoutes')(app);
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
