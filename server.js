31a6084d875fdbe09865c27616a649b6ad7960b2
acquahsamuel <acquahsamuel1996@gmail.com>
2021-08-05 08:46:39 +0000

    server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 const connectDB = require('./config/db');
-dotenv.config({ path : './config/config.env' })
+dotenv.config({ path : './config.env' })
 connectDB();
 
 
@@ -11,11 +11,11 @@ require('./services/passport');
 require('./routes/authRoutes')(app);
 
 
-
-
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
     `App is runing on ${PORT} hosted on Heroku`;
 });
+
+
 
 
